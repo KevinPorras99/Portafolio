2611a6d625860e5eddf4c93514c883bd78c72600
Kevin Porras <kevinporras9906@gmail.com>
2025-01-13 15:39:15 -0600

    Cambio de nombre

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/', // Asegúrate de que esta ruta coincida con el nombre de tu repositorio
+  base: '/Portfolio', // Asegúrate de que esta ruta coincida con el nombre de tu repositorio
   plugins: [react()],
 });
