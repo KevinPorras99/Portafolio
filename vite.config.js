dcaafaa7535d2deabd36966df81bca2eb54ad992
Kevin Porras <kevinporras9906@gmail.com>
2025-01-13 15:08:10 -0600

    Update configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Portafolio/', // Asegúrate de que esta ruta coincida con el nombre de tu repositorio
   plugins: [react()],
-})
+});
