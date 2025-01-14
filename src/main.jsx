fb6f55a2055459e9c67a3599d4d8616a9dfc1c71
Kevin Porras <kevinporras9906@gmail.com>
2025-01-13 19:04:36 -0600

    Update configuration for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Portafolio">
       <App />
     </BrowserRouter>
   </StrictMode>
