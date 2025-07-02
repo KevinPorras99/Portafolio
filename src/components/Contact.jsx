05a240fc950ca243ba18dee3d3f7905214e5ef38
Kevin Porras <kevinporras9906@gmail.com>
2025-07-01 18:41:14 -0600

    Cambios de link de linkedin

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => (
       <div className="contact-card-content">
         <p><FontAwesomeIcon icon={faPhone} /> Phone: (+506)8645-7832</p>
         <p><FontAwesomeIcon icon={faEnvelope} /> Email: kevinporras9906@gmail.com</p>
-        <p><FontAwesomeIcon icon={faLinkedin} /> LinkedIn: <a href="https://www.linkedin.com/in/kevin-porras-3a8bab253/" target="_blank" rel="noopener noreferrer">LinkedIn Profile</a></p>
+        <p><FontAwesomeIcon icon={faLinkedin} /> LinkedIn: <a href="https://www.linkedin.com/in/kevin-porras99/" target="_blank" rel="noopener noreferrer">LinkedIn Profile</a></p>
         <p><FontAwesomeIcon icon={faGithub} /> GitHub: <a href="https://github.com/KevinPorras99" target="_blank" rel="noopener noreferrer">GitHub Profile</a></p>
       </div>
     </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => (
       <a href="https://github.com/KevinPorras99" target="_blank" rel="noopener noreferrer" className="btn-social">
         <i className="fa fa-github"></i> 
       </a>
-      <a href="https://www.linkedin.com/in/kevin-porras-3a8bab253/" target="_blank" rel="noopener noreferrer" className="btn-social">
+      <a href="https://www.linkedin.com/in/kevin-porras99/" target="_blank" rel="noopener noreferrer" className="btn-social">
         <i className="fa fa-linkedin"></i> 
       </a>
       <a href="mailto:kevinporras9906@gmail.com" className="btn-social">
